969e821c7b01b3e92a3a19aa1fcff52a2c1abff4
peter <pkh879@gmail.com>
2020-09-11 13:59:03 +0900

    Create repository to github

---
+++ src/test/java/com/example/webfluxmicroservicedemo/WebfluxMicroserviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webfluxmicroservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxMicroserviceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
